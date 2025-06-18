2471927a182a48d913b9b65e8e6909cfb8f611d7
duyguabbasoglu <duygua963@gmail.com>
2025-06-18 14:29:00 +0300

    sb eklendi

---
+++ src/main/java/com/kuputhane/kuputhane/KuputhaneApplication.java
@@ -0,0 +1,13 @@
+package com.kuputhane.kuputhane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuputhaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuputhaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuputhane/kuputhane/KuputhaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuputhane.kuputhane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuputhaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
