ea345e85322b2ffda907d58cdb436b66b9357977
duyguabbasoglu <duygua963@gmail.com>
2025-07-09 15:48:00 +0300

    hintli dayi olcam

--- payment-service/src/main/java/com/kuputhane/paymentservice/config/CorsConfig.java
+++ payment-service/src/main/java/com/kuputhane/paymentservice/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8082")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
